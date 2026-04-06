292d26def0277957912b72b42b46373265e8fbe5
tmfittro <tmfittro@uiowa.edu>
2026-04-05 21:35:50 -0500

    flask-app updates

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 # module 10
 # taryn fittro 4/5/26
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!!"
+    return render_template('index.html')
+
+@app.route("/about")
+def about_page():
+    return render_template('about.html')
 
 if __name__ == "__main__":
-      app.run(host="0.0.0.0", port=5002, debug=True)
+    app.run(host="0.0.0.0", port=5002, debug=True)
